8a3b851bd389ed0819e7615fac8fca81f6cab872
Nate <n.mantoes@gmail.com>
2018-01-17 02:14:28 -0500

    Update LineageBuild.sh

--- LineageBuild.sh
+++ LineageBuild.sh
@@ -26,6 +26,7 @@ repo init -u https://github.com/LineageOS/android.git -b cm-14.1 --depth=1
 repo sync
 
 mkdir -p .repo/local_manifests
+# This is the only device-specific command of this script:
 wget https://raw.githubusercontent.com/sigmarelax/LineageOSscripts/master/RIOandroid_manifest.xml -O .repo/local_manifests/android_manifest.xml
 repo sync
 
@@ -33,3 +34,5 @@ export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G
 source build/envsetup.sh
 breakfast rio
 brunch rio
+
+# The flashable .zip will be at ~/android/lineage/out/target/product/rio/lineage-14.1-2018XXXX-UNOFFICIAL-rio.zip
